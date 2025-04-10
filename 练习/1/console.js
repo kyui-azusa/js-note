2268c0b8c1c9f29814275a7a22353bdb32b9b9b1
kyui-azusa <1817054005@qq.com>
2025-04-10 17:57:33 +0800

    first commit

---
+++ 练习/1/console.js
@@ -0,0 +1,7 @@
+// console.log(console.log())
+
+// var1 = '变量1'
+// var2 = 'variable2'
+// console.log(`var1: ${var1} var2: ${var2}`)
+console.log(console)
+console.log('This is %cstyled%c text!', 'color: red; font-weight: bold', 'color: black');
